8a3d76f754ae596a5dc6378231130ca606683fbd
anush91 <anushm@packt.com>
2019-10-25 11:17:33 +0530

    Update

--- Chapter 13/red-fill-paint.js
+++
@@ -1,8 +0,0 @@
-class RedFillPaintWorklet {
-    paint(context, geometry) {
-        context.fillStyle = "red";
-        context.fillRect(0, 0, geometry.width, geometry.height);
-    }
-}
-
-registerPaint("red-fill", RedFillPaintWorklet);
